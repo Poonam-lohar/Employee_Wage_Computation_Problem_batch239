f9c34f7176b23ee560250c5e993bed85ef5bf182
Your Name <you@example.com>
2023-01-07 18:58:19 +0530

    Added: Calculate employee daily wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -6,12 +6,18 @@ public class EmployeeWage {
 
         System.out.println("...Welcome To Employee Wage Computation Program...");
 
-        int empPresent = (int) (Math.floor(Math.random() * 10) % 2);
-        if(empPresent == 1){
-            System.out.println("Employee is Present");
-        }
-        else{
-            System.out.println("Employee is Absent");
+         final int WAGE_PER_HOUR = 20;
+         final int FULL_DAY_HOUR = 8;
+
+
+            int empPresent = (int) (Math.floor(Math.random() * 10) % 2);
+            int empWage = 0;
+            if (empPresent == 1) {
+                empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                System.out.println("Employee is Present");
+            } else {
+                System.out.println("Employee is Absent");
+            }
+            System.out.println("Employee daily wage: " + empWage);
         }
     }
-}
