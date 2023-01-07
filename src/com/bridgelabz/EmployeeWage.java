424eaf5a276a10fe9e5ebb8f9816ba73e93a0465
Your Name <you@example.com>
2023-01-07 19:01:45 +0530

    Added: Check Employee present part time or full time

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -8,16 +8,20 @@ public class EmployeeWage {
 
          final int WAGE_PER_HOUR = 20;
          final int FULL_DAY_HOUR = 8;
+         final int PART_TIME__HOUR = 4;
 
-
-            int empPresent = (int) (Math.floor(Math.random() * 10) % 2);
+            int empPresent = (int) (Math.floor(Math.random() * 10) % 3);
             int empWage = 0;
-            if (empPresent == 1) {
+            if (empPresent == 2) {
                 empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                System.out.println("Employee is Present");
+                System.out.println("Employee FULL-TIME");
+            } else if (empPresent == 1) {
+                empWage = WAGE_PER_HOUR * PART_TIME__HOUR;
+                System.out.println("Employee PART-TIME");
             } else {
                 System.out.println("Employee is Absent");
             }
+
             System.out.println("Employee daily wage: " + empWage);
         }
     }
