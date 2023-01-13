0bd4798f1cb6c4e222712d1896340ed539b7efd4
Your Name <you@example.com>
2023-01-13 10:08:04 +0530

    Added: To save total wage for each company

---
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -0,0 +1,56 @@
+package com.bridgelabz;
+
+public class EmpWageBuilder {
+
+    static final int FULL_DAY_HOUR = 8;
+    static final int PART_TIME_HOUR = 4;
+    static final int IS_FULL_TIME = 2;
+    static final int IS_PART_TIME = 1;
+
+    String company;
+    int wagePerHour;
+    int workingDayPerMonth;
+    int workingHourPerMonth;
+    int totalWage;
+
+    public EmpWageBuilder(String company, int wagePerHour, int workingDayPerMonth, int workingHourPerMonth) {
+        this.company = company;
+        this.wagePerHour = wagePerHour;
+        this.workingDayPerMonth = workingDayPerMonth;
+        this.workingHourPerMonth = workingHourPerMonth;
+    }
+
+
+void computeEmpWage(){
+        int empPresent = (int) (Math.floor((Math.random() * 10) % 3));
+        int dayCount = 1;
+        int workingHrs = 0;
+
+        while (dayCount <= workingDayPerMonth && workingHrs <= workingHourPerMonth) {
+
+            int empWage = 0;
+            switch (empPresent) {
+
+                case IS_FULL_TIME:
+                    empWage = wagePerHour * FULL_DAY_HOUR;
+                    workingHrs += FULL_DAY_HOUR;
+                    System.out.println("Employee Full-Time");
+                    break;
+                case IS_PART_TIME:
+                    empWage = wagePerHour * PART_TIME_HOUR;
+                    workingHrs += PART_TIME_HOUR;
+                    System.out.println("Employee is Absent");
+                    break;
+                default:
+                    System.out.println("Employee daily wage : " + dayCount + " == " + empWage);
+                    dayCount++;
+            }
+            System.out.println("Working Hours: " + workingHrs);
+        }
+        totalWage = workingHrs * wagePerHour;
+}
+    @Override
+    public String toString() {
+        return "Total wage for company: " + company + " is " +totalWage;
+    }
+}
