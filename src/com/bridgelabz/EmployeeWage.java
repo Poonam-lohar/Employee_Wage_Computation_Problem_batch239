b7eb9c84763d0da749352ae1c3eafa8b226c87fd
Your Name <you@example.com>
2023-01-20 14:26:23 +0530

    Added: Use company emp wage class

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,12 +5,11 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         System.out.println("Welcome To Employee Wage Computation Program");
-        EmpWageBuilder amazon = new EmpWageBuilder("Amazon",20,2,10);
-        EmpWageBuilder reliance = new EmpWageBuilder("reliance",10,4,20);
-        amazon.computeEmpWage();
-        System.out.println(amazon);
-        reliance.computeEmpWage();
-        System.out.println(reliance);
-
+        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+        empWageBuilder.addCompanyEmpWage ("Amazon",20,2,10);
+        empWageBuilder.addCompanyEmpWage("reliance",10,4,20);
+        empWageBuilder.addCompanyEmpWage("DMart",10,4,20);
+        empWageBuilder.addCompanyEmpWage("FlipCart",15,5,30);
+        empWageBuilder.computeEmpWage();
     }
 }
